429e8a451359892bf69084eda8a6ab2ecf2c2cd4
Nigel Gregoire <nigelgregoire@gmail.com>
2020-12-03 22:26:56 -0800

    Solve day 4 part 2 in node

--- 04/node.js
+++ 04/node.js
@@ -1,42 +1,93 @@
 const fs = require('fs');
-const util = require('util');
 
-const field_regex = /^([^:]+):(.*)$/;
+const passport_fields = [
+  {
+    name: "byr", // Birth Year
+    required: true,
+    validation: val => Number.parseInt(val) && val >= 1920 && val <= 2002
+  },
+  {
+    name: "iyr", // Issue Year
+    required: true,
+    validation: val => Number.parseInt(val) && val >= 2010 && val <= 2020
+  },
+  {
+    name: "eyr", // Expiration Year
+    required: true,
+    validation: val => Number.parseInt(val) && val >= 2020 && val <= 2030
+  },
+  {
+    name: "hgt", // Height
+    required: true,
+    validation: val => {
+      const matched = val.match(/(\d+)(cm|in)/);
+      if (! matched) { return false; }
+      if (matched[2] === 'cm') {
+        return matched[1] >= 150 && matched[1] <= 193;
+      }
+      return matched[1] >= 59 && matched[1] <= 76;
+    },
+  },
+  {
+    name: "hcl", // Hair Colour
+    required: true,
+    // a # followed by exactly six characters 0-9 or a-f.
+    validation: val => Boolean(val.match(/^#[0-9a-f]{6}$/)) 
+  },
+  {
+    name: "ecl", // Eye Colour
+    required: true,
+    validation: val => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+      .filter(ele => val === ele)
+      .length === 1
+  },
+  {
+    name: "pid", // Passport ID
+    required: true,
+    validation: val => Boolean(val.match(/^\d{9}$/))
+  },
+  {
+    name: "cid", // Country ID
+    required: false,
+    validation: val => true
+  }
+];
 
 function parse_raw_password(raw_passport) {
   // Example passport data.
   //
   // ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
   // byr:1937 iyr:2017 cid:147 hgt:183cm
-  const fields = raw_passport.split(/\s/).map(ele => {
-    const found = ele.match(field_regex);
+  return raw_passport.split(/\s/).reduce((acc, ele) => {
+    const found = ele.match(/^([^:]+):(.*)$/);
     if (! found) {
       throw Error(`Couldn't parse passport: ${raw_passport}`);
     }
-    return [found[1], found[2]]; // [key, value].
-  });
-  return Object.fromEntries(fields);
+    acc[found[1]] = found[2]
+    return acc;
+  },{}); // acc = object
 }
 
-//const required_fields = [ "ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt" ];
-const required_fields = [ "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt" ];
-function solve_part_one(passports) {
+function solve(passports, do_validation) {
   return passports.filter(passport => {
     let result = true;
-    required_fields.forEach(field => {
-      if (passport[field] === undefined) {
-        result = false;
+    for ({name, required, validation} of passport_fields) {
+      let value = passport[name];
+      if (required && value === undefined) {
+        return false;
       }
-    })
-    return result;
-  }).length;
+      if (do_validation && ! validation(value)) {
+        return false;
+      }
+    }
+    return true;
+  }).length
 }
 
 fs.readFile('input', {encoding: "UTF-8"}, (err, data) => {
   if (err) { throw err }
 
-
   const passports = data.trimEnd().split("\n\n").map(parse_raw_password);
-
-  console.log(`Answer to part one: ${solve_part_one(passports)}`);
+  console.log(`Answer to part one: ${solve(passports, false)}`);
+  console.log(`Answer to part two: ${solve(passports, true)}`);
 });
